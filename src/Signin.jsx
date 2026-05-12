53a306f68e773e2955465bb38ddf520834cfe1b8
ravi <pixel.photoo23@gmail.com>
2026-05-12 11:05:06 +0800

    test

--- src/Signin.jsx
+++ src/Signin.jsx
@@ -52,13 +52,12 @@ export default function SignIn({ onGoLogin }) {
   };
 
   return (
-    <div className="login-shell">
+    <div>
       <div className="login-card">
 
         <img src={logoConnecta} alt="Connecta" className="login-logo" />
         <h2 className="login-title">CUSTOMER SIGN IN</h2>
 
-        {/* 🔥 ERROR BOX UI */}
         {error && (
           <div className="error-box">
             ⚠ {error}
